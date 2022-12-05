697e9e874115a6f97bef98b0bfc5b79b65020213
Mateusz Murawski <303840@office.umcs.pl>
2022-12-05 15:51:37 +0100

    poprawki na hosting

--- Server/main.py
+++ Server/main.py
@@ -1,36 +1,12 @@
-#!/usr/bin/env python
-
 import asyncio
 import http
 import signal
-
 import websockets
 
+from server import Server
 
-async def echo(websocket):
-    async for message in websocket:
-        await websocket.send(message)
-
-
-async def health_check(path, request_headers):
-    if path == "/healthz":
-        return http.HTTPStatus.OK, [], b"OK\n"
-
-
-async def main():
-    # Set the stop condition when receiving SIGTERM.
-    loop = asyncio.get_running_loop()
-    stop = loop.create_future()
-    loop.add_signal_handler(signal.SIGTERM, stop.set_result, None)
-
-    async with websockets.serve(
-        echo,
-        host="",
-        port=8080,
-        process_request=health_check,
-    ):
-        await stop
-
-
-if __name__ == "__main__":
-    asyncio.run(main())
+if __name__ == '__main__':
+    event_loop = asyncio.get_event_loop()
+    event_loop.run_until_complete(Server.startServer())
+    event_loop.run_forever()
+   
