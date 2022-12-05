dbbfed454bafd3d51110aef32809da753c06bbb4
Mateusz Murawski <303840@office.umcs.pl>
2022-12-05 15:59:44 +0100

    poprawki na hosting

--- Server/main.py
+++ Server/main.py
@@ -5,8 +5,13 @@ import websockets
 
 from server import Server
 
+async def main():
+    loop = asyncio.get_running_loop()
+    stop = loop.create_future()
+    loop.add_signal_handler(signal.SIGTERM, stop.set_result, None)
+
+    await Server.startServer()
+
 if __name__ == '__main__':
-    event_loop = asyncio.get_event_loop()
-    event_loop.run_until_complete(Server.startServer())
-    event_loop.run_forever()
+    asyncio.run(main())
    
